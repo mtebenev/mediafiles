e109e73b2813dbda0de2604a91b9dad3cf351dc7
Maxim Tebenev <mtebenev@gmail.com>
2019-07-29 19:01:20 +0300

    Added some controls

---
+++ ArtefactUi.Uwp/ExplorerControl.xaml.cs
@@ -0,0 +1,14 @@
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ArtefactUi.Uwp
+{
+  public sealed partial class ExplorerControl: UserControl
+  {
+    public ExplorerControl()
+    {
+      this.InitializeComponent();
+    }
+  }
+}
