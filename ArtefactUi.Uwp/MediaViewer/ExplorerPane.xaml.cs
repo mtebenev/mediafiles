e17df58dd8eeb6b0507bceb0b1477f035fbcac19
Maxim Tebenev <mtebenev@gmail.com>
2020-01-30 21:07:27 +0300

    Arefactory UWP: Added bound prop

--- ArtefactUi.Uwp/MediaViewer/ExplorerPane.xaml.cs
+++ ArtefactUi.Uwp/MediaViewer/ExplorerPane.xaml.cs
@@ -1,3 +1,5 @@
+using Mt.MediaMan.ArtefactUi.Uwp.Core;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
@@ -6,9 +8,24 @@ namespace Mt.MediaMan.ArtefactUi.Uwp.MediaViewer
 {
   public sealed partial class ExplorerPane : UserControl
   {
+    private static readonly DependencyProperty _dpMediaItem =
+    DependencyProperty.Register(nameof(BoundProp), typeof(MediaItem), typeof(ViewerPane), new PropertyMetadata(0));
+
     public ExplorerPane()
     {
       this.InitializeComponent();
     }
+
+    // MediaItem DP
+    public static DependencyProperty BoundPropProperty => _dpMediaItem;
+    public string BoundProp
+    {
+      get => (string)GetValue(BoundPropProperty);
+      set
+      {
+        SetValue(BoundPropProperty, value);
+      }
+    }
+
   }
 }
