de06ade59477aa9c8c8c17a8455cbaa9be5d16f9
Maxim Tebenev <mtebenev@gmail.com>
2020-01-30 21:17:48 +0300

    Added rnw

---
+++ ArtefactUi/windows/ArtefactUi/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.ReactNative;
+
+namespace ArtefactUi
+{
+    sealed partial class App : ReactApplication
+{
+    public App()
+    {
+        MainComponentName = "ArtefactUi";
+
+#if BUNDLE
+        JavaScriptBundleFile = "index.windows";
+        InstanceSettings.UseWebDebugger = false;
+        InstanceSettings.UseLiveReload = false;
+#else
+        JavaScriptMainModuleName = "index";
+        InstanceSettings.UseWebDebugger = true;
+        InstanceSettings.UseLiveReload = true;
+#endif
+
+#if DEBUG
+        InstanceSettings.EnableDeveloperMenu = true;
+#else
+        InstanceSettings.EnableDeveloperMenu = false;
+#endif
+
+        PackageProviders.Add(new Microsoft.ReactNative.Managed.ReactPackageProvider()); // Includes any modules in this project
+
+        InitializeComponent();
+    }
+}
+}
