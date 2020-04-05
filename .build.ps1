9a1f96d254cf26f6cc55d3174e9f5db439381b8e
Maxim Tebenev <mtebenev@gmail.com>
2020-04-05 10:01:29 +0300

    Added build scripts

---
+++ .build.ps1
@@ -0,0 +1,26 @@
+# Synopsis: Build the app.
+task Build-App {
+  exec {
+      dotnet @(
+        "build",
+        "-v",
+        "minimal",
+        "-c",
+        "Release"
+      )
+  }
+}
+
+# Synopsis: Build the Chocolatey package.
+task Build-Package {
+  New-Item -ItemType Directory -Force -Path artifacts | Out-Null
+  cd chocolatey
+  exec {
+      choco @(
+        "pack",
+        "mediafiles.nuspec",
+        "--out",
+        "../artifacts"
+      )
+  }
+}
